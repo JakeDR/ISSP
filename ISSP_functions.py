b2a9dce73ce257138877d167c3e8db78294a0fc8
Isaac Reich <ireich@cnc.local>
2017-12-18 20:28:38 +0000

    Added content list + comment corrections

--- ISSP_functions.py
+++ ISSP_functions.py
@@ -1,4 +1,13 @@
-#ISSP functions and class definitions
+#ISSP functions
+
+# altitude()
+# airDensity()
+# drag()
+# burnFuel()
+# thrustVector1()
+# thrustVector2()
+# gravity()
+# netGravity()
 
 ######################################
 
@@ -12,15 +21,15 @@ def altitude(point, planet, r):
 
 # returns density of air (kg/m^3) at given altitude above earth
 def airDensity(alt):
-	
+
 	p0 = 101.325  # sea level standard atmospheric pressure, kPa
 	T0 = 288.15  # sea level standard temperature, K
 	g = 9.80665  # earth surface gravitation acceleration, m/s^2
-	L = 0.0065  # temerature lapse rate, K/m
+	L = 0.0065  # temperature lapse rate, K/m
 	R = 8.31447  # ideal gas constant, J/(mol*K)
 	M = 0.0289644  # molar mass of dry air, kg/mol
-	
-	
+
+
 	if alt >= 42000:  # if alt is greater than 42,000m, assume out of atmosphere
 		density = 0
 		return density
@@ -30,7 +39,7 @@ def airDensity(alt):
 		density = (p*M)/(R*T)
 		return density
 
-# use below if wanting to visulise relationship
+# use below if wanting to visualise relationship
 #alt = np.arange(0,45000, 10)
 #density = np.zeros(len(alt))
 
@@ -71,17 +80,17 @@ def burnFuel(dt, burnRate, throttle, fuelMass):
 ######################################
 
 # function to generate a thrust vector to specifiy in which direction the engines will
-# produce force. The thrust vecot us is 3-component vector, where sqrt(x^2 + y^2 + z^2) = 1.
-# Therefore simplying multiplying to engine's total force by this vector gives the force
+# produce force. The thrust vector is 3-component vector, where sqrt(x^2 + y^2 + z^2) = 1.
+# Therefore simply multiplying the engine's total force by this vector gives the force
 # in [x, y, z].
 # This simple version allows absolute thrust vector to be given as either in the spacecraft's
 # direction of travel, or against it.
 def thrustVector1(v, direction):
-	
+
 	if direction == 'fwd':  # thrust to be in spacecraft direction of travel
 		thrustVector = v / (v[0]**2 + v[1]**2 + v[2]**2)**0.5
 		return thrustVector
-		
+
 	if direction == 'rev':  # thrust to be against spacecraft direction of travel
 		thrustVector = -v / (v[0]**2 + v[1]**2 + v[2]**2)**0.5
 		return thrustVector
@@ -92,14 +101,14 @@ def thrustVector1(v, direction):
 # produce force. This more complex function uses the spacecrafts current velocity as one
 # axis, and the line from the spacecraft to some other point as the other axis. The third
 # axis is then orthogonal to these two axes.
-# angle1 is the angle 
+# angle1 is the angle
 def thrustVector2(v, pos, reference_pos, yaw, elevation):
 	# okay, got a bit stuck on this one! JR
 	return
 
 ######################################
 
-# Calculate component force [Fx, Fy, Fz] felt by body at pos1, with mass1, due to 
+# Calculate component force [Fx, Fy, Fz] felt by body at pos1, with mass1, due to
 # gravitional attraction with body at pos2 with mass2
 # pos1 = position of body1 [x, y, z] (m)
 # pos2 = position of body2 [x, y, z] (m)
@@ -110,22 +119,22 @@ def gravity(pos1, pos2, mass1, mass2):
 	d = ((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2 + (pos1[2] - pos2[2])**2)**0.5
 	F = ((G*mass1*mass2)/d**3)*(pos2 - pos1)
 	return F  # component force felt by mass at pos1
-	
+
 ######################################
 
 # given a list containing all bodies in the simulation, calculate the net force due
 # to gravity for each body, due to gravity between all other bodies.
 def netGravity(bodyList):
-	
+
 	for i in range(0, len(bodyList)):  # for as many times as there are bodies
-		
+
 		bodyList[0].gravity = np.array([0, 0, 0])  # clear previous net gravity of the first body
-		
+
 		for k in range(1, len(bodyList)):  # for each body in list except the first one
-			
+
 			# calculate net gravity between first body in list, and all others
 			bodyList[0].gravity = bodyList[0].gravity + gravity(bodyList[0].pos, bodyList[i].pos, bodyList[0].totalMass, bodyList[i].totalMass)
-		
+
 		bodyList = np.roll(bodyList, 1)  # rotate bodyList by 1 to present new body at bodyList[0]
 
 ######################################
